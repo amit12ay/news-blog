dea2af2f72bb5a406707eb0ad644c6755191b7b7
amitay7096 <71865210+amitay7096@users.noreply.github.com>
2022-11-22 17:58:38 +0545

    First Commit

---
+++ news-blog/src/components/home/mainContent/home/sideContent/side/Side.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Heading from '../../../../../common/heading/Heading'
+import SocialMedia from './social/SocialMedia'
+
+const Side = () => {
+  return (
+   <>
+   <Heading title='Stay Connected' />
+   <SocialMedia/>
+   </>
+  )
+}
+
+export default Side
+ 
---
+++ news-blog/src/components/home/mainContent/home/sideContent/side/social/SocialMedia.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SocialMedia = () => {
+  return (
+ <>
+ <section className='social'>
+    <div className='socBox'>
+        <i className="fab fa-facebook-f"></i>
+        <spam >12,740 Likes</spam>
+    </div>
+    <div className='socBox'>
+        <i className="fab fa-facebook-f"></i>
+        <spam >12,740 Likes</spam>
+    </div>
+ </section>
+ </>
+  )
+}
+
+export default SocialMedia
